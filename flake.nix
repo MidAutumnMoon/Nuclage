4c2b73bedfb8367bde8969bc585a2bcdc593a82d
MidAutumnMoon <me@418.im>
2022-04-07 16:14:23 +0800

    override: pin firefox to an older version

--- flake.nix
+++ flake.nix
@@ -6,10 +6,18 @@
   inputs =
     {
 
-      nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable-small";
-      nulib.url = "github:MidAutumnMoon/Nulib";
+      nixpkgs.url =
+        "github:NixOS/nixpkgs/nixos-unstable-small";
+
+      nulib.url =
+        "github:MidAutumnMoon/Nulib";
+
+      slightly-older-nixpkgs.url =
+        "nixpkgs/04ce3788d37dc3f5ab1b156f2a817c8e7630b3b4";
+
 
       # Overlays from other projects by MidAutumnMoon
+
       Opah =
         { url = "github:MidAutumnMoon/Opah";
           inputs.nixpkgs.follows = "nixpkgs";
@@ -19,37 +27,51 @@
     };
 
 
-  outputs = { self, nixpkgs, ... } @ flake:
+  outputs = { self, nixpkgs, nulib, ... } @ flake:
 
     let
 
-      system = "x86_64-linux";
+      lib =
+        nixpkgs.lib.extend nulib.lib;
+
+      pkgsForSystem =
+        lib.importNixpkgs {
+          inherit nixpkgs;
+          overlays = self.totalOverlays;
+        };
+
+
+      # Firefox 99 breaks itself.
+
+      olderPkgsForSystems =
+        lib.importNixpkgs { nixpkgs = flake.slightly-older-nixpkgs; };
+
+      overrideFirefoxOverlay = self: super:
+        {
+          inherit (olderPkgsForSystems."${self.system}")
+            firefox-unwrapped;
+        };
 
     in
 
     {
       # Softwares packaged by Nuclage.
-      #
+
       overlay = import ./default.nix;
 
 
       # Nuclage's overlay plus other projects'
-      #
+
       totalOverlays = with flake;
         [
+          overrideFirefoxOverlay
+
           Opah.overlay
           self.overlay
         ];
 
 
-      # For tesing the packages.
-      #
-      packages.${system} = import nixpkgs
-        {
-          inherit system;
-          overlays = self.totalOverlays;
-        };
-
+      packages = pkgsForSystem lib.id;
     };
 
 }
