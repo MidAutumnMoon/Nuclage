e9760cdb3ab57e0caf32151e1191f3b84a1f8b4d
MidAutumnMoon <me@418.im>
2022-06-18 01:20:03 +0800

    languages: zuo update

--- languages/zuo/default.nix
+++ languages/zuo/default.nix
@@ -3,10 +3,10 @@
 let
 
   rev =
-    "82ca0f76f2e18f242db742991596eb509ce49cc1";
+    "fa746e3bb1be105499a5f80a8b846bf2bc27c826";
 
-  sha256 =
-    "sha256-j+Pg64Y5sxcjMtUkeM4Bjz8XRZiHGRcxhhjWZSV7aB0=";
+  hash =
+    "sha256-v/I/RVC1WEm0dW5HGNL36rgWZq7xcchBcOWG3SDTcFU=";
 
   zuoRoot =
     "racket/src/zuo";
@@ -19,7 +19,7 @@ stdenv.mkDerivation ( drvSelf: {
   version = "unstable";
 
   src = fetchFromGitHub
-    { inherit rev sha256;
+    { inherit rev hash;
       owner = "racket";
       repo = "racket";
       sparseCheckout =
